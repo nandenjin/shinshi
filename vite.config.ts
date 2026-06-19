f5a233ecc7ed718450d975980bb86e869db87135
Kazumi <hello@nandenjin.com>
2026-06-19 14:49:44 +0000

    chore: Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+const base = process.env.BASE_PATH;
+if (base) {
+  console.log(`Using base path: ${base}`);
+}
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base,
 });
